43c25bdfd9bd932e0a419de0afd708faedd577f6
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-05-24 20:02:49 +0600

    products display

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Product from "./Product";
+
 export default function App() {
   return (
     <>
-      <h1 className="bg-yellow-600 text-center font-bold p-4 text-3xl" >Kishor Kumar Paroi</h1>
+      <Product />
     </>
   )
 }
