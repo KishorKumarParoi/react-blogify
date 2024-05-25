96931627b3591d91574fa988816b207032ab322c
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2024-05-25 21:51:01 +0600

    crud project in react-router

--- src/main.jsx
+++ src/main.jsx
@@ -1,36 +1,58 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import Root, { action as rootAction, loader as rootLoader } from "./routes/root";
 
-import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
-import About from './About.jsx';
-import ContactUs from './ContactUs.jsx';
-import Github, { githubInfoLoader } from './Github.jsx';
-import Home from './Home.jsx';
-import Layout from './Layout';
-import NotFound from './NotFound.jsx';
-import User from './User.jsx';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import Contact, { action as contactAction, loader as contactLoader } from "./routes/Contact";
+import { action as destroyAction } from "./routes/Destroy";
+import EditContact, { action as editAction } from "./routes/Edit";
+import ErrorPage from "./routes/ErrorPage";
+import Index from "./routes/Index";
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={<Layout />}>
-      <Route path='' element={<Home />} />
-      <Route path='about' element={<About />} />
-      <Route path='contact' element={<ContactUs />} >
-        <Route path='user/:userId' element={<User />} />
-      </Route>
-      <Route
-        loader={githubInfoLoader}
-        path='github'
-        element={<Github />}
-      />
-      <Route path='*' element={<NotFound />} />
-    </Route>
-  )
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            index: true,
+            element: <Index />,
+          },
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>,
+          },
+        ],
+      }
+    ]
+  },
+]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
